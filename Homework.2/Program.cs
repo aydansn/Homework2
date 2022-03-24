b74bdff9910d4127f0825d15108e87344c571dbd
aydansn <aydansn@code.edu.az>
2022-03-24 21:58:07 +0400

    Add project files.

---
+++ Homework.2/Program.cs
@@ -0,0 +1,276 @@
+﻿using System;
+
+namespace Homework._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int a = 9999;
+            //int b = 70000;
+            //int c = 8;
+            //int eded = ((b + a) * 10) + c;
+            //Console.WriteLine(eded);
+
+
+            #endregion
+
+            #region Task2
+            //int a = 333;
+            //int b = (a * 1000) + a;
+
+            //Console.WriteLine(b);
+            #endregion
+
+            #region Task3
+            //int a = 10000;
+            //int b = (a / 100) * 18;
+            //int c = (a / 100) * 3;
+
+            //Console.WriteLine($"Ededin 18%-i budur:{b}");
+            //Console.WriteLine($"Ededin 3%-i budur:{c}");
+
+            //int d = (a / 100) * 18;
+            //int e = (d / 100) * 3;
+
+            //Console.WriteLine($"Ededin 18%-nin 3%-i budur: {e}");
+            #endregion
+
+            #region Task4
+            //int a = 111;
+            //Console.WriteLine(a);
+            //int b = 111 * 10 + 7;
+            //Console.WriteLine(b);
+            //int d = (b / 100) * 7;
+            //Console.WriteLine(d);
+
+            #endregion
+
+            #region Task5
+
+            //int a = 1111;
+            //Console.WriteLine(a);
+            //int b = 40000;
+            //int c = (a + b) * 100 + 44;
+            //Console.WriteLine(c);
+            //int d = (c / 100) * 44;
+            //Console.WriteLine(d);
+
+            #endregion
+
+            #region Task6
+            //int a = 1111;
+            //int b = (a / 100) * 20;
+            //Console.WriteLine(b);
+
+            //int c = (b / 100) * 10;
+            //Console.WriteLine(c);
+
+            //int d = (c * c);
+            //Console.WriteLine(d);
+            #endregion
+
+            #region Task7
+            //int a = 10000;
+            //int b = 11111;
+            //int c = a + b;
+            //Console.WriteLine(c);
+
+            //int d = 5000000 + ((c * 10) + 5);
+            //Console.WriteLine(d);
+
+            //int f = (d / 100) * 5;
+            //Console.WriteLine(f);
+            #endregion
+
+            #region Task8
+
+            //int a = 1000;
+            //int b = 1000000;
+            //int c = (a / 100) * 4;
+            //Console.WriteLine(c);
+
+            //int d = (b / 100) * 9;
+            //Console.WriteLine(d);
+            //int e = (c + d);
+            //Console.WriteLine(e);
+
+            //int f = (e / 100) * 10;
+            //Console.WriteLine(f);
+            #endregion
+
+            #region Task9
+            //   int a = 100000;
+            //   int b = 111111;
+            //   int c = 222222;
+
+            //   int d = (a / 100) * 10;
+            //   Console.WriteLine(d);
+            //   int e = (b / 100) * 10;
+            //   Console.WriteLine(e);
+            //   int f = (c / 100) * 10;
+            //   Console.WriteLine(f);
+
+            //   int g = (d + e + f);
+            //   Console.WriteLine(g);
+
+            //int h = (g / 100) * 10;
+            //   Console.WriteLine(h);
+            #endregion
+
+            #region Task10
+            //int a = 1000;
+            //int b = 1111;
+            //int c = 2222;
+            //int d = (a / 100) * 1;
+            //Console.WriteLine(d);
+            //int e = (b / 100) * 2;
+            //Console.WriteLine(e);
+            //int f = (c / 100) * 3;
+            //Console.WriteLine(f);
+            //int g = (f - e - d);
+            //Console.WriteLine(g);
+            //int h =  g + (c / 100) * 7;
+            //Console.WriteLine(h);
+            #endregion
+
+            #region Task11
+            //int a = 10000;
+            //int b = 11111;
+            //int c = 22222;
+            //int d = 33333;
+            //int e = (a + c);
+            //Console.WriteLine(e);
+
+            //int f = (b + d);
+            //Console.WriteLine(f);
+
+            //int g = (e * f);
+            //Console.WriteLine(g);
+
+            //int h = g - (c / 100) * 3;
+            //Console.WriteLine(h);
+            #endregion
+
+            #region Task 12
+            //int a = 100000;
+            //int b = 111111;
+            //int c = 222222;
+            //int d = 333333;
+            //int e = (a / 100) * 10;
+            //Console.WriteLine(e);
+            //int f = (b / 100) * 10;
+            //Console.WriteLine(f);
+            //int g = (c / 100) * 10;
+            //Console.WriteLine(g);
+            //int h = (d / 100) * 10;
+            //Console.WriteLine(h);
+
+            //int i = (e + f + g + h);
+            //Console.WriteLine(i);
+
+            //int k = (a / 100) * 10;
+            //Console.WriteLine(k);
+            //int l = (b / 100) * 15;
+            //Console.WriteLine(l);
+            //int m = (c / 100) * 15;
+            //Console.WriteLine(m);
+            //int n = (d / 100) * 15;
+            //Console.WriteLine(n);
+
+            //int s = (e + f + g + h);
+            //Console.WriteLine(s);
+
+            //int q = (i * s);
+            //Console.WriteLine(q);
+            //int r = (q / 100) * 10;
+            //Console.WriteLine(r);
+            //int t = (r / 100) * 11;
+            //Console.WriteLine(t);
+            #endregion
+
+            #region Task 13
+            //int a = 10000;
+            //int b = 11111;
+            //int c = 22222;
+            //int d = 100;
+            //int e = 111;
+            //int f = (a / 100) * 5;
+            //Console.WriteLine(f);
+            //int g = (b / 100) * 5;
+            //Console.WriteLine(g);
+            //int h = (c / 100) * 5;
+            //Console.WriteLine(h);
+            //int i = (f * g * h);
+
+            //Console.WriteLine(i);
+
+            //int j = (d / 100) * 3;
+            //Console.WriteLine(j);
+            //int k = (e / 100) * 3;
+            //Console.WriteLine(k);
+            //int l = (j + k);
+
+            //Console.WriteLine(l);
+
+            //int m = (i / 100) * 10;
+            //Console.WriteLine(m);
+            //int n = (l / 100) * 10;
+            //Console.WriteLine(n);
+            //int o = (m + n);
+            //Console.WriteLine(o);
+            #endregion
+
+            #region Task14
+            //int a = 10000;
+            //int b = 11111;
+            //int c = 22222;
+            //int d = 33333;
+            //int e = 44444;
+            //int f = 55555;
+            //int g = (a + b + c + d + e + f);
+            //Console.WriteLine(g);
+            //int h = (a * 100000) + c;
+            //Console.WriteLine(h);
+
+            #endregion
+
+            #region Task15
+            //int a = 100;
+            //int b = 111;
+            //int c = 1000;
+            //int d = 1111;
+            //int e = 10000;
+            //int f = 11111;
+            //int g = 100000;
+            //int h = a + b;
+            //Console.WriteLine(h);
+            //int i = (c * d) + h;
+            //Console.WriteLine(i);
+            //int j = (i * 10) + 7;
+            //Console.WriteLine(j);
+            //int k = (j + e + f);
+            //Console.WriteLine(k);
+            //int l = k - ((a * b) * 10 + 1);
+            //Console.WriteLine(l);
+            //int m = l + g;
+            //Console.WriteLine(m);
+            //int n = m - (a + b + c + d);
+            //Console.WriteLine(n);
+            //int o = (n / 100) * 18;
+            //int p = (o / 100) * 3;
+            //int q = (p / 100) * 1;
+            //Console.WriteLine(q);
+            //int r = (q + e + f);
+            //Console.WriteLine(r);
+            #endregion
+
+
+
+
+
+
+        }
+    }
+}
